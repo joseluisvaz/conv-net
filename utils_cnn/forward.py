8ba1a9bcacc52da7b45d21b400f16294cf0bf963
Jose Vazquez <jvazquez@crabi.com>
2017-11-29 01:58:05 -0600

    added forward functions for convolutional layer and pooling layer (#4)

---
+++ utils_cnn/forward.py
@@ -0,0 +1,137 @@
+import numpy as np
+from utils import zero_pad
+
+def convolution_single_step(A_prev_slice, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation
+    of the previous layer.
+
+    Arguments:
+    A_prev_slice -- slice of input data of shape (f, f, n_C_prev)
+    W -- Weight parameters contained in a window - matrix of shape (f, f, n_C_prev)
+    b -- Bias parameters contained in a window - matrix of shape (1, 1, 1)
+
+    Returns:
+    Z -- a scalar value, result of convolving the sliding window (W, b) on a slice x of the input data
+    """
+    s = np.multiply(A_prev_slice, W)
+    z = np.sum(s)
+    z = z + float(b) #Casting float type to Z for scalar output
+
+    # Return Z to pass it through the RELU function
+    return z
+
+def conv_layer(A_prev, W, b, parameters):
+    """
+        Implements the forward propagation for a convolution function
+
+        Arguments:
+        A_prev -- output activations of the previous layer, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+        W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+        b -- Biases, numpy array of shape (1, 1, 1, n_C)
+        parameters -- python dictionary containing "stride" and "pad"
+            parameters["pad"] => contains the value for the pad
+            parameters["stride"] => contains the value for the stride
+        Returns:
+        Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
+        cache -- cache of values needed for the conv_backward() function
+    """
+
+    # Retrieve dimensions from A_prev's shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    # Retrieve dimensions from W's shape
+    (f, f, n_C_prev, n_C) = W.shape
+    # Retrieve information from "hparameters"
+    stride = parameters["stride"]
+    pad = parameters["pad"]
+    # Compute the dimensions of the CONV output volume using the formula given above. Hint: use int() to floor.
+    n_H = int(np.floor((n_H_prev - f + 2 * pad) / stride) + 1)
+    n_W = int(np.floor((n_W_prev - f + 2 * pad) / stride) + 1)
+    # Initialize the output volume Z with zeros.
+    Z = np.zeros((m, n_H, n_W, n_C))
+    # Create A_prev_pad by padding A_prev
+    A_prev_pad = zero_pad(A_prev, pad)
+
+    for i in range(m):  # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]  # Select ith training example's padded activation
+        for h in range(n_H):  # loop over vertical axis of the output volume
+            for w in range(n_W):  # loop over horizontal axis of the output volume
+                for c in range(n_C):  # loop over channels (= #filters) of the output volume
+
+                    # Find the corners of the current "slice"
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
+
+                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell).
+                    a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+                    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron.
+                    Z[i, h, w, c] = conv_layer(a_slice_prev, W[:, :, :, c], b[:, :, :, c])
+
+    # Making sure your output shape is correct
+    assert (Z.shape == (m, n_H, n_W, n_C))
+
+    # Save information in "cache" for the backprop
+    cache = (A_prev, W, b, parameters)
+
+    return Z, cache
+
+
+def pool_forward(A_prev, parameters, mode="max"):
+    """
+    Implements the forward pass of the pooling layer
+
+    Arguments:
+    A_prev -- Input data, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    hparameters -- python dictionary containing "f" and "stride"
+    mode -- the pooling mode you would like to use, defined as a string ("max" or "average")
+
+    Returns:
+    A -- output of the pool layer, a numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache used in the backward pass of the pooling layer, contains the input and hparameters
+    """
+
+    # Retrieve dimensions from the input shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    # Retrieve hyperparameters from "parameters"
+    f = parameters["f"]
+    stride = parameters["stride"]
+
+    # Define the dimensions of the output
+    n_H = int(1 + (n_H_prev - f) / stride)
+    n_W = int(1 + (n_W_prev - f) / stride)
+    n_C = n_C_prev
+
+    # Initialize output matrix A
+    A = np.zeros((m, n_H, n_W, n_C))
+
+    for i in range(m):  # loop over the training examples
+        for h in range(n_H):  # loop on the vertical axis of the output volume
+            for w in range(n_W):  # loop on the horizontal axis of the output volume
+                for c in range(n_C):  # loop over the channels of the output volume
+
+                    # Find the corners of the current "slice"
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
+
+                    # Use the corners to define the current slice on the ith training example of A_prev, channel c.
+                    a_prev_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+
+                    # Compute the pooling operation on the slice. Use an if statment to differentiate the modes. Use np.max/np.mean.
+                    if mode == "max":
+                        A[i, h, w, c] = np.max(a_prev_slice)
+                    elif mode == "average":
+                        A[i, h, w, c] = np.mean(a_prev_slice)
+
+    # Store the input and hparameters in "cache" for pool_backward()
+    cache = (A_prev, parameters)
+
+    # Making sure your output shape is correct
+    assert (A.shape == (m, n_H, n_W, n_C))
+
+    return A, cache
+
