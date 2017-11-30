e56436a4d7336a2a0e8f1492bf60d4e25898fe07
Jose Luis <joseluis.vaz.e@gmail.com>
2017-11-29 23:55:02 -0600

    implemented MaxPool Backward pass, testing in test notebook

--- utils_cnn/backward.py
+++ utils_cnn/backward.py
@@ -60,3 +60,57 @@ def convolutional_backward(dZ, cache):
     assert (dA_prev.shape == (m, n_H_prev, n_W_prev, n_C_prev))
 
     return dA_prev, dW, db
+
+
+def pool_backward(dA, cache, mode="max"):
+    """
+    Implements the backward pass of the pooling layer
+
+    Arguments:
+    dA -- gradient of cost with respect to the output of the pooling layer, same shape as A
+    cache -- cache output from the forward pass of the pooling layer, contains the layer's input and hparameters
+    mode -- the pooling mode you would like to use, defined as a string ("max" or "average")
+
+    Returns:
+    dA_prev -- gradient of cost with respect to the input of the pooling layer, same shape as A_prev
+    """
+
+    def create_mask_from_window(x):
+        return x == np.max(x)
+
+    def distribute_value(dz, shape):
+        return dz / (shape[0] * shape[1]) * np.ones(shape)
+
+    (A_prev, hparameters) = cache
+    stride = hparameters["stride"]
+    f = hparameters["f"]
+
+    m, n_H_prev, n_W_prev, n_C_prev = A_prev.shape
+    m, n_H, n_W, n_C = dA.shape
+
+    dA_prev = np.zeros(A_prev.shape)
+
+    for i in range(m):  # loop over the training examples
+        a_prev = A_prev[i]
+        for h in range(n_H):  # loop on the vertical axis
+            for w in range(n_W):  # loop on the horizontal axis
+                for c in range(n_C):  # loop over the channels (depth)
+
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
+
+                    if mode == "max":
+                        a_prev_slice = a_prev[vert_start:vert_end, horiz_start:horiz_end, c]
+                        mask = create_mask_from_window(a_prev_slice)
+                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += mask * dA[i, h, w, c]
+
+                    elif mode == "average":
+                        da = dA[i, h, w, c]
+                        shape = (f, f)
+                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += distribute_value(da, shape)
+
+    assert (dA_prev.shape == A_prev.shape)
+
+    return dA_prev
--- utils_cnn/forward.py
+++ utils_cnn/forward.py
@@ -18,7 +18,6 @@ def convolution_single_step(A_prev_slice, W, b):
     z = np.sum(s)
     z = z + float(b) #Casting float type to Z for scalar output
 
-    # Return Z to pass it through the RELU function
     return z
 
 def conv_layer(A_prev, W, b, parameters):
@@ -37,19 +36,15 @@ def conv_layer(A_prev, W, b, parameters):
         cache -- cache of values needed for the conv_backward() function
     """
 
-    # Retrieve dimensions from A_prev's shape
     (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
-    # Retrieve dimensions from W's shape
     (f, f, n_C_prev, n_C) = W.shape
-    # Retrieve information from "hparameters"
     stride = parameters["stride"]
     pad = parameters["pad"]
-    # Compute the dimensions of the CONV output volume using the formula given above. Hint: use int() to floor.
     n_H = int(np.floor((n_H_prev - f + 2 * pad) / stride) + 1)
     n_W = int(np.floor((n_W_prev - f + 2 * pad) / stride) + 1)
-    # Initialize the output volume Z with zeros.
+
     Z = np.zeros((m, n_H, n_W, n_C))
-    # Create A_prev_pad by padding A_prev
+
     A_prev_pad = zero_pad(A_prev, pad)
 
     for i in range(m):  # loop over the batch of training examples
@@ -57,22 +52,14 @@ def conv_layer(A_prev, W, b, parameters):
         for h in range(n_H):  # loop over vertical axis of the output volume
             for w in range(n_W):  # loop over horizontal axis of the output volume
                 for c in range(n_C):  # loop over channels (= #filters) of the output volume
-
-                    # Find the corners of the current "slice"
                     vert_start = h * stride
                     vert_end = vert_start + f
                     horiz_start = w * stride
                     horiz_end = horiz_start + f
-
-                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell).
                     a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
-                    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron.
                     Z[i, h, w, c] = convolution_single_step(a_slice_prev, W[:, :, :, c], b[:, :, :, c])
 
-    # Making sure your output shape is correct
     assert (Z.shape == (m, n_H, n_W, n_C))
-
-    # Save information in "cache" for the backprop
     cache = (A_prev, W, b, parameters)
 
     return Z, cache
@@ -92,19 +79,13 @@ def pool_forward(A_prev, parameters, mode="max"):
     cache -- cache used in the backward pass of the pooling layer, contains the input and hparameters
     """
 
-    # Retrieve dimensions from the input shape
     (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
-
-    # Retrieve hyperparameters from "parameters"
     f = parameters["f"]
     stride = parameters["stride"]
-
-    # Define the dimensions of the output
     n_H = int(1 + (n_H_prev - f) / stride)
     n_W = int(1 + (n_W_prev - f) / stride)
     n_C = n_C_prev
 
-    # Initialize output matrix A
     A = np.zeros((m, n_H, n_W, n_C))
 
     for i in range(m):  # loop over the training examples
@@ -112,25 +93,19 @@ def pool_forward(A_prev, parameters, mode="max"):
             for w in range(n_W):  # loop on the horizontal axis of the output volume
                 for c in range(n_C):  # loop over the channels of the output volume
 
-                    # Find the corners of the current "slice"
                     vert_start = h * stride
                     vert_end = vert_start + f
                     horiz_start = w * stride
                     horiz_end = horiz_start + f
 
-                    # Use the corners to define the current slice on the ith training example of A_prev, channel c.
                     a_prev_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
 
-                    # Compute the pooling operation on the slice. Use an if statment to differentiate the modes. Use np.max/np.mean.
                     if mode == "max":
                         A[i, h, w, c] = np.max(a_prev_slice)
                     elif mode == "average":
                         A[i, h, w, c] = np.mean(a_prev_slice)
 
-    # Store the input and hparameters in "cache" for pool_backward()
     cache = (A_prev, parameters)
-
-    # Making sure your output shape is correct
     assert (A.shape == (m, n_H, n_W, n_C))
 
     return A, cache
