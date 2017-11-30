d96b73b5140f295ea9dd86b71bd3249bb309b7cd
Jose Luis <joseluis.vaz.e@gmail.com>
2017-11-29 18:35:40 -0600

    added testing notebook and load dataset utils

--- utils_cnn/utils.py
+++ utils_cnn/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import h5py
 
 def zero_pad(X, pad):
     """
@@ -19,4 +20,22 @@ def zero_pad(X, pad):
     return X_pad
 
 
+def load_dataset():
+    train_dataset = h5py.File('data/train_signs.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
 
+    test_dataset = h5py.File('data/test_signs.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)].T
+    return Y
